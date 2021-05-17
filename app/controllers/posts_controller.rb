00023659c3030c59a1d9b127db89ff6fc3e48fc6
yuya yamada <yuya19920721@gmail.com>
2021-05-17 20:49:25 +0900

    コントローラの共通処理をメソッド化したため修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,15 +19,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: 'つぶやきを編集しました。'
     else
