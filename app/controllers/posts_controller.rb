e2f61cadae444f5ccc0cf5efc56448c0cf40348c
yuya yamada <yuya19920721@gmail.com>
2021-05-17 13:43:33 +0900

    パラメーターをストロングパラメーターへ。汎用性の高い処理なのでプライベート配下でメソッド定義する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,21 @@
 class PostsController < ApplicationController
+
   def index
+
   end
+
   def new
     @post = Post.new
   end
+
   def create
-    
+    Post.create(post_params)
+    redirect_to new_post_path, notice: 'つぶやきました。'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
